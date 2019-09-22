7c5b26b8bdb7a62edbefdcf65989b0d90b4dd58f
MerijnHendriks <54717671+MerijnHendriks@users.noreply.github.com>
2019-09-22 08:05:52 +0200

    initial inheritance code

---
+++ inheritance/main.c
@@ -0,0 +1,43 @@
+/* INHERITANCE
+ * Authors:        Merijn Hendriks
+ * License:        MIT License
+ * Documentation:  see readme.md
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct A
+{
+    int value;
+};
+
+struct B
+{
+    struct A baseA;
+    int value;
+};
+
+static void A_ctor(struct A* self, int a)
+{
+    self->value = a;
+}
+
+static void B_ctor(struct B* self, int a, int b)
+{
+    A_ctor(&self->baseA, a);
+
+    self->value = self->baseA.value + b;
+}
+
+int main()
+{
+    struct B b;
+    
+    B_ctor(&b, 1, 2);
+    
+    printf("Value of b: %d\n", b.value);
+    printf("Value of a: %d\n", b.baseA.value);
+    
+    return EXIT_SUCCESS;
+}
